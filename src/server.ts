ea32f845509da6a7799fa3aa5d734081330bd0c4
Vitor Carrilho <vitor.carrilho@hotmail.com>
2021-12-29 22:26:19 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+const PORT = 3000;
+
+const add = (...numbers: number[]): number => {
+  return numbers.reduce((accumulator, current) => {
+    return (accumulator += current);
+  });
+};
+
+app.get("/", (req: Request, res: Response) => {
+  let teste = add(55, 55, 100, 500);
+  res.send(`${teste}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor executando na porta ${PORT}.`);
+});
